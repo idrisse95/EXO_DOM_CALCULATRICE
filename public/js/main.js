8f9444dee0c20495927129a886e53b54e7f38252
55973 <55973@etu.he2b.be>
2023-01-19 14:09:01 +0100

    fin du trois

--- public/js/main.js
+++ public/js/main.js
@@ -22,10 +22,11 @@ let case4 = document.getElementById('case4')
 console.log(case3, case4);
 let select = document.querySelector('select')
 console.log(select);
-let rep2 = document.getElementById('response3')
+let rep2 = document.getElementById('response2')
+let egale2 = document.getElementById('egale2')
 
 
-select.addEventListener('click', () =>{
+egale2.addEventListener('click', () =>{
 
 
     switch (select.value) {
@@ -33,6 +34,76 @@ select.addEventListener('click', () =>{
             rep2.innerHTML = parseInt(case3.value) + parseInt(case4.value)
             
             break;
+
+            case "-" :
+                rep2.innerHTML = parseInt(case3.value) - parseInt(case4.value)
+                
+                break;
+
+                case "/" :
+                    rep2.innerHTML = parseInt(case3.value) / parseInt(case4.value)
+                    
+                    break;
+
+                    case "*" :
+                        rep2.innerHTML = parseInt(case3.value) * parseInt(case4.value)
+                        
+                        break;
+    
+        default:
+            alert('ddd')
+            break;
+    }
+})
+
+
+let case5 = document.querySelector('#operatorsExo3 input')
+console.log(case5);
+let case6 = document.querySelectorAll('#operatorsExo3 input')[1]
+console.log(case6);
+
+let span = document.querySelector('#operatorsExo3 span i')
+let buttonSum = document.querySelectorAll('#operatorsExo3 button')[1]
+let buttonSous = document.querySelectorAll('#operatorsExo3 button')[2]
+let buttonMulti = document.querySelectorAll('#operatorsExo3 button')[3]
+let buttonDiv = document.querySelectorAll('#operatorsExo3 button')[4]
+let equals3 = document.querySelector('#operatorsExo3 button')
+let rep3 = document.getElementById('response3')
+console.log(buttonSum,buttonSous,buttonMulti,buttonDiv,equals3);
+
+
+buttonSum.addEventListener('click',()=>{
+    span.setAttribute('class','fas fa-plus')
+})
+
+buttonSous.addEventListener('click',()=>{
+    span.setAttribute('class','fas fa-minus')
+})
+
+buttonDiv.addEventListener('click',()=>{
+    span.setAttribute('class','fas fa-divide')
+})
+
+buttonMulti.addEventListener('click',()=>{
+    span.setAttribute('class','fas fa-times')
+})
+
+equals3.addEventListener('click',()=>{
+
+    switch (span.className) {
+        case "fas fa-times":
+            rep3.innerHTML= parseInt(case5.value)*parseInt(case6.value)
+            break;
+        case "fas fa-divide":
+            rep3.innerHTML= parseInt(case5.value)/parseInt(case6.value)
+            break;
+        case "fas fa-minus":
+            rep3.innerHTML= parseInt(case5.value)-parseInt(case6.value)
+            break;
+        case "fas fa-plus":
+            rep3.innerHTML= parseInt(case5.value)+parseInt(case6.value)
+            break;
+
     
         default:
             break;
